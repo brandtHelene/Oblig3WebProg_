c59200adc634ecca62135a4ce9b02bb9cabe1446
HeleneBrandt <144004013+HeleneBrandt@users.noreply.github.com>
2024-04-21 09:53:48 +0200

    fix bug and connect to h2-concole

--- src/main/java/kinoprogram/kinooblig/KinoObligApplication.java
+++ src/main/java/kinoprogram/kinooblig/KinoObligApplication.java
@@ -1,15 +1,28 @@
 package kinoprogram.kinooblig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KinoObligApplication {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(KinoObligApplication.class);
 
+    static {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            logger.error("Kunne ikke finne JDBC-driverklassen", e);
+
+        }
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(KinoObligApplication.class, args);
     }
 
 }
+
 
